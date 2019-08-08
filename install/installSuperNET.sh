1ca8896aa095d2afc3ec9b9787fe2c5ea33337c6
blackjok3r <blackjok3r@gmail.com>
2019-08-09 07:28:22 +0800

    .

--- install/installSuperNET.sh
+++ install/installSuperNET.sh
@@ -2,5 +2,4 @@
 cd ~
 sudo apt-get update
 sudo apt-get -y install jq htop tmux git slurm bc dc
-git clone https://github.com/KMDLabs/SuperNET.git -b blacktest
-
+git clone https://github.com/KMDLabs/SuperNET.git -b recvmask_check
