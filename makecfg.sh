55db4f27877e2618eb9b2b7b0032dd7b7ba90722
blackjok3r <blackjok3r@gmail.com>
2019-08-09 03:28:03 +0800

    .

--- makecfg.sh
+++ makecfg.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-radd=$(komodo-cli -ac_name=TEST1 getnewaddress)
-pubkey=$(komodo-cli -ac_name=TEST1 validateaddress $radd | jq -r .pubkey)
-wif=$(komodo-cli -ac_name=TEST1 dumpprivkey $radd)
+cd "${BASH_SOURCE%/*}" || exit
+cli="$HOME/StakedNotary/komodo/master/komodo-cli -ac_name=TEST1"
+radd=$($cli getnewaddress)
+pubkey=$($cli validateaddress $radd | jq -r .pubkey)
+wif=$($cli dumpprivkey $radd)
 cfgtemp=$(<config_example.ini)
-./asset-cli setpubkey $pubkey > /dev/null &2>1
+$cli setpubkey $pubkey > /dev/null &2>1
 echo "$cfgtemp" | sed "s/RADD/$radd/" | sed "s/PUBKEY/$pubkey/" | sed "s/WIF/$wif/"  > config_test.ini
 echo "$pubkey"
 echo "$radd"
