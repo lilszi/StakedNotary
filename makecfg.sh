ce67bce12a8ff0f080722ea351bdd6a59a356f45
blackjok3r <blackjok3r@gmail.com>
2019-08-09 03:21:34 +0800

    .

--- makecfg.sh
+++ makecfg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-radd=$(komdodo-cli -ac_name=TEST1 getnewaddress)
-pubkey=$(komdodo-cli -ac_name=TEST1 validateaddress $radd | jq -r .pubkey)
-wif=$(komdodo-cli -ac_name=TEST1 dumpprivkey $radd)
+radd=$(komodo-cli -ac_name=TEST1 getnewaddress)
+pubkey=$(komodo-cli -ac_name=TEST1 validateaddress $radd | jq -r .pubkey)
+wif=$(komodo-cli -ac_name=TEST1 dumpprivkey $radd)
 cfgtemp=$(<config_example.ini)
 ./asset-cli setpubkey $pubkey > /dev/null &2>1
 echo "$cfgtemp" | sed "s/RADD/$radd/" | sed "s/PUBKEY/$pubkey/" | sed "s/WIF/$wif/"  > config_test.ini
