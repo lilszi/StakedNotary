b29dea03eed6f5b9bc32dacf2a0fd2d38098a7df
blackjok3r <blackjok3r@gmail.com>
2019-08-15 02:15:59 +0800

    bn

---
+++ blocknotify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ~/StakedNotary
+
+./listassetchains.py | while read coin; do
+  echo $coin
+  conf="$HOME/.komodo/$coin/$coin.conf"
+  if [[ -f $conf ]]; then
+    echo "blocknotify=curl -s --url \"http://127.0.0.1:7776\" --data \"{\\\"agent\\\":\\\"dpow\\\",\\\"method\\\":\\\"updatechaintip\\\",\\\"blockhash\\\":\\\"%s\\\",\\\"symbol\\\":\\\"$ac\\\"}\"" >> $conf
+  else
+     echo "failed"
+   fi 
+done
