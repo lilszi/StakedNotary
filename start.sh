47532b9e440ee0f89619295638c5a7bcecffb897
blackjok3r <blackjok3r@gmail.com>
2019-08-15 14:52:22 +0800

    fix

--- start.sh
+++ start.sh
@@ -85,8 +85,10 @@ daemon_stopped () {
   else
     pidfile="$HOME/.komodo/$1/komodod.pid"
     conf="$HOME/.komodo/$1/$1.conf"
-    sed -i '/blocknotify/d' $conf
-    echo "blocknotify=curl -s --url \"http://127.0.0.1:7776\" --data \"{\\\"agent\\\":\\\"dpow\\\",\\\"method\\\":\\\"updatechaintip\\\",\\\"blockhash\\\":\\\"%s\\\",\\\"symbol\\\":\\\"$1\\\"}\"" >> ${conf}
+    if [[ -f $conf ]]; then
+        sed -i '/blocknotify/d' $conf
+        echo "blocknotify=curl -s --url \"http://127.0.0.1:7776\" --data \"{\\\"agent\\\":\\\"dpow\\\",\\\"method\\\":\\\"updatechaintip\\\",\\\"blockhash\\\":\\\"%s\\\",\\\"symbol\\\":\\\"$1\\\"}\"" >> ${conf}
+    fi
   fi
   while [[ -f $pidfile ]]; do
     pid=$(cat $pidfile 2> /dev/null)
